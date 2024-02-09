20e9828af6a556806ab319ee2c79dc9d715bc8d8
Nicolas Rol <nicolas.rol@rte-france.com>
2024-02-09 13:36:53 +0100

    add test on error
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

--- computation-slurm/src/test/java/com/powsybl/computation/slurm/FlagFilesMonitorTest.java
+++ computation-slurm/src/test/java/com/powsybl/computation/slurm/FlagFilesMonitorTest.java
@@ -6,7 +6,11 @@
  */
 package com.powsybl.computation.slurm;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -14,6 +18,8 @@ import java.util.Arrays;
 import java.util.List;
 
 import static com.powsybl.computation.slurm.CommandResultTestFactory.multilineOutput;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 /**
@@ -53,5 +59,44 @@ class FlagFilesMonitorTest {
         verify(runner, times(1)).execute("rm /flagdir/myerror_workingDir_1234_2");
         verify(jobs.get(0), times(1)).done();
         verify(jobs.get(1), times(1)).failed();
+    }
+
+    @Test
+    void testError() {
+        ListAppender<ILoggingEvent> logWatcher = new ListAppender<>();
+        logWatcher.start();
+        ((Logger) LoggerFactory.getLogger(AbstractSlurmJobMonitor.class)).addAppender(logWatcher);
+
+        String wdName = "workingDir_1234";
+        CommandExecutor runner = mock(CommandExecutor.class);
+        when(runner.execute("testError -1 /flagdir"))
+            .thenReturn(multilineOutput(Arrays.asList("mydone_" + wdName + "_1",
+                "myerror_" + wdName + "_2")));
+
+        TaskStore taskStore = mock(TaskStore.class);
+
+        List<MonitoredJob> jobs = new ArrayList<>();
+        for (long i = 1; i < 7; i++) {
+            MonitoredJob job = mock(MonitoredJob.class);
+            when(job.getJobId()).thenReturn(i);
+            jobs.add(job);
+        }
+        when(taskStore.getPendingJobs()).thenReturn(jobs);
+
+        Path flagDir = mock(Path.class);
+        when(flagDir.toString()).thenReturn("/flagdir");
+        SlurmComputationManager cm = mock(SlurmComputationManager.class);
+        when(cm.getTaskStore()).thenReturn(taskStore);
+        when(cm.getCommandRunner()).thenReturn(runner);
+        when(cm.getFlagDir()).thenReturn(flagDir);
+
+        FlagFilesMonitor sut = new FlagFilesMonitor(cm);
+        sut.run();
+
+        // Checks
+        List<ILoggingEvent> logsList = logWatcher.list;
+        assertEquals(1, logsList.size());
+        assertEquals("Exception in job state detection",
+            logsList.get(0).getFormattedMessage());
     }
 }
