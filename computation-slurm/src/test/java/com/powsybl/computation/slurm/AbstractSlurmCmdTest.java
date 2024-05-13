fb3f55b273b160e7ca78514b0b602a9d6bed9a45
Nicolas Rol <nicolas.rol@rte-france.com>
2024-05-13 13:33:59 +0200

    add coverage to AbstractSlurmCmd
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

---
+++ computation-slurm/src/test/java/com/powsybl/computation/slurm/AbstractSlurmCmdTest.java
@@ -0,0 +1,42 @@
+package com.powsybl.computation.slurm;
+
+import com.powsybl.computation.ComputationParameters;
+import com.powsybl.computation.ExecutionEnvironment;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static com.powsybl.computation.slurm.CommandExecutionsTestFactory.md5sumLargeFile;
+import static com.powsybl.computation.slurm.CommandResultTestFactory.simpleOutput;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.startsWith;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Nicolas Rol {@literal <nicolas.rol at rte-france.com>}
+ */
+class AbstractSlurmCmdTest extends DefaultSlurmTaskTest {
+
+    @Test
+    void testException() throws IOException {
+        CommandExecutor commandExecutor = mock(CommandExecutor.class);
+        when(commandExecutor.execute(startsWith("sbatch")))
+            .thenReturn(new CommandResult(-1, "stdOut", "stdErr"))
+            .thenReturn(simpleOutput("Submitted batch job 2"))
+            .thenReturn(simpleOutput("Submitted batch job 5"))
+            .thenReturn(simpleOutput("Submitted batch job 6"));
+        SlurmTask task = new JobArraySlurmTask(mockScm(commandExecutor), mockWd(), md5sumLargeFile(), ComputationParameters.empty(), ExecutionEnvironment.createDefault());
+        try {
+            task.submit();
+            fail();
+        } catch (SlurmException exception) {
+            String message = "com.powsybl.computation.slurm.SlurmCmdNonZeroException: \n" +
+                "exitcode:-1\n" +
+                "err:stdErr\n" +
+                "out:stdOut";
+            assertEquals(message, exception.getMessage());
+        }
+    }
+}
